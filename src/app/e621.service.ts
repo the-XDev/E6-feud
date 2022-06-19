89298eb851d7a43b5c644d8d14f79663e1c0e55b
XDev <28283142+the-XDev@users.noreply.github.com>
2022-06-19 17:21:20 +0100

    Added client description to API requests

--- src/app/e621.service.ts
+++ src/app/e621.service.ts
@@ -13,10 +13,10 @@ export class E621Service {
   getSinglePost(tags : string) : Observable<PostsRoot>{
     tags = tags.replace(/\s/g, '+');
     // for random, add the order:random tag
-    return this.http.get<PostsRoot>('https://e621.net/posts.json?tags='+tags+'&limit=1');
+    return this.http.get<PostsRoot>('https://e621.net/posts.json?tags='+tags+'&limit=1&_client=E6Feud');
   }
 
   getTagAliases(tag : string) : Observable<any[]>{ // wip
-    return this.http.get<any[]>('https://e621.net/tag_aliases?format=json&search[name_matches]='+tag+'&search[status]=active');
+    return this.http.get<any[]>('https://e621.net/tag_aliases?format=json&search[name_matches]='+tag+'&search[status]=active&_client=E6Feud');
   }
 }
